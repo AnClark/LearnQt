0c645e0ebd9793cd89bb5764b557656a655e73a9
AnClark <anclark@hust.edu.cn>
2016-03-29 19:50:22 +0800

    Homework on March 29th

---
+++ MsgBox_demo/mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //连接信号和槽
+    connect(ui->btnInformation,SIGNAL(clicked()),this,SLOT(slotInformation()));
+    connect(ui->btnQuestion,SIGNAL(clicked()),this,SLOT(slotQuestion()));
+    connect(ui->btnWarning,SIGNAL(clicked()),this,SLOT(slotWarning()));
+    connect(ui->btnCritical,SIGNAL(clicked()),this,SLOT(slotCritical()));
+    connect(ui->btnAbout,SIGNAL(clicked()),this,SLOT(slotAbout()));
+    connect(ui->btnAboutQt,SIGNAL(clicked()),this,SLOT(slotAboutQt()));
+    connect(ui->btnCustom,SIGNAL(clicked()),this,SLOT(slotCustom()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//写槽函数
+void MainWindow::slotInformation()
+{
+    QMessageBox::information(this,"信息对话框","欢迎感受Qt的对话框！");
+}
+
+void MainWindow::slotWarning()
+{
+    QMessageBox::warning(this,"警告对话框","你还有未保存的文件，请马上保存！",QMessageBox::Ok | QMessageBox::Cancel);
+}
+
+void MainWindow::slotQuestion()
+{
+    QMessageBox::question(this,"问题对话框","您要使用我们的推荐配置吗？",QMessageBox::Yes | QMessageBox::No);
+}
+
+void MainWindow::slotCritical()
+{
+    QMessageBox::critical(this,"出错对话框","程序崩溃，即将重启！",QMessageBox::Abort | QMessageBox::Retry | QMessageBox::Cancel);
+}
+
+void MainWindow::slotAbout()
+{
+    QMessageBox::about(this,"关于","爱拼安小匠，匠心独运，爱拼会赢！");
+}
+
+void MainWindow::slotAboutQt()
+{
+    QMessageBox::aboutQt(this);
+}
+
+void MainWindow::slotCustom()
+{
+    //Dummy code here
+}
---
+++ MsgBox_demo/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QMessageBox>               //必须先定义QMessageBox，才能调出对话框！
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void slotInformation();
+    void slotQuestion();
+    void slotWarning();
+    void slotCritical();
+    void slotAbout();
+    void slotAboutQt();
+    void slotCustom();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
